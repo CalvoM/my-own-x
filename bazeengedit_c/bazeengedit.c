a1d95dbd6e6f417a3b83382be599161781a3e241
Calvin Mwadime Makokha <mwadimemakokha@gmail.com>
2024-02-25 20:43:21 +0300

    bazeengedit.c: arrow cursor movement

--- bazeengedit_c/bazeengedit.c
+++ bazeengedit_c/bazeengedit.c
@@ -12,8 +12,11 @@
 #define ABUF_INIT                                                              \
   { NULL, 0 }
 
+enum editor_key { ARROW_LEFT = 1000, ARROW_RIGHT, ARROW_UP, ARROW_DOWN };
+
 /*** data ***/
 struct editor_config {
+  int cursor_x, cursor_y;
   int screen_rows;
   int screen_cols;
   struct termios orig_termios;
@@ -52,14 +55,35 @@ void enable_raw_mode() {
   }
 }
 
-char editor_read_key() {
+int editor_read_key() {
   int nread;
   char c;
   while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
     if (nread == -1 && errno != EAGAIN)
       die("read");
   }
-  return c;
+  if (c == '\x1b') {
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) != 1)
+      return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1)
+      return '\x1b';
+    if (seq[0] == '[') {
+      switch (seq[1]) {
+      case 'A':
+        return ARROW_UP;
+      case 'B':
+        return ARROW_DOWN;
+      case 'C':
+        return ARROW_RIGHT;
+      case 'D':
+        return ARROW_LEFT;
+      }
+    }
+    return '\x1b';
+  } else {
+    return c;
+  }
 }
 
 int get_window_size(int *rows, int *cols) {
@@ -89,13 +113,35 @@ void ab_append(struct editor_abuf *e_ab, const char *s, int len) {
 
 void ab_free(struct editor_abuf *e_ab) { free(e_ab->b); }
 /*** input ***/
+void editor_move_cursor(int key) {
+  switch (key) {
+  case ARROW_LEFT:
+    E.cursor_x--;
+    break;
+  case ARROW_RIGHT:
+    E.cursor_x++;
+    break;
+  case ARROW_UP:
+    E.cursor_y--;
+    break;
+  case ARROW_DOWN:
+    E.cursor_y++;
+    break;
+  }
+}
 void editor_process_key_press() {
-  char c = editor_read_key();
+  int c = editor_read_key();
   switch (c) {
   case CTRL_KEY('q'):
     write(STDOUT_FILENO, "\x1b[2J", 4);
     write(STDOUT_FILENO, "\x1b[H", 3);
     exit(0);
+    break;
+  case ARROW_UP:
+  case ARROW_DOWN:
+  case ARROW_LEFT:
+  case ARROW_RIGHT:
+    editor_move_cursor(c);
     break;
   }
 }
@@ -135,12 +181,19 @@ void editor_refresh_screen() {
   ab_append(&e_ab, "\x1b[H", 3);
   editor_draw_rows(&e_ab);
   ab_append(&e_ab, "\x1b[H", 3);
+  char buf[32];
+  int buf_len =
+      snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cursor_y + 1, E.cursor_x + 1);
+  ab_append(&e_ab, buf, buf_len);
   ab_append(&e_ab, "\x1b[?25h", 6);
   write(STDOUT_FILENO, e_ab.b, e_ab.len);
+  ab_free(&e_ab);
 }
 
 /*** init **/
 void init_editor() {
+  E.cursor_x = 20;
+  E.cursor_y = 20;
   if (get_window_size(&E.screen_rows, &E.screen_cols) == -1)
     die("get_window_size");
 }
